2cfe35ccdde6aa89f05fc6963ebdc35bbeb4d914
Peng <1a84@proton.me>
2023-08-04 14:34:06 +0200

    increase image size; turn it back to dark

--- components/ProductName.js
+++ components/ProductName.js
@@ -1,11 +1,10 @@
-import { Avatar, Box } from "grommet";
-import Image from "next/image";
-import React from "react";
+import { Avatar, Box } from 'grommet';
+import React from 'react';
 
 function ProductName({ name, logo, link }) {
   return (
     <Box direction="row" align="center" pad="4px 0">
-      {!!logo && <Avatar src={logo} alt={name} border={{ color: "white" }} />}
+      {!!logo && <Avatar src={logo} alt={name} border={{ color: "white" }} size="large" />}
 
       <Box pad={logo ? "0 0 0 12px" : "0"}>{link}</Box>
     </Box>
